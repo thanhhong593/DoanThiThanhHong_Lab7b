068890da27dabedb0025adc8fce936defdeebc29
Your Name <thanhhong593@gmail.com>
2021-05-17 01:25:23 +0700

    Second

--- app/src/main/java/com/example/lab7_b/MainActivity.java
+++ app/src/main/java/com/example/lab7_b/MainActivity.java
@@ -3,31 +3,82 @@ package com.example.lab7_b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    ArrayList<City> arrList = new ArrayList<>();
-    CityAdapter adt;
+    public ArrayList<City> arrList = new ArrayList<>();
+    City city ;
+
     Button btnSave;
     Button btnCancel;
     EditText txtName;
-    EditText txtID;
+    ArrayAdapter adt;
+    ImageButton btnImgDelete;
+    ImageButton btnImgEdit;
+    ArrayList idList;
+
+    DatabaseHandler databaseHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listView);
-        DatabaseHandler databaseHandler= new DatabaseHandler(MainActivity.this);
+        listView = findViewById(R.id.listview);
+         databaseHandler= new DatabaseHandler(MainActivity.this);
+        idList = new ArrayList();
+        btnImgEdit =findViewById(R.id.imgEdit);
+        btnSave = findViewById(R.id.btnadd);
+        txtName = findViewById(R.id.edtinput);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name =txtName.getText().toString();
+                int max=arrList.size();
+                databaseHandler.saveCity(new City(max+1,name));
+                recreate();
+
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                btnImgDelete = (ImageButton) view.findViewById(R.id.imgDelete);
+                btnImgDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        databaseHandler.deleteStudent(position);
+                        Toast.makeText(MainActivity.this, "Xoá place thành công!!!", Toast.LENGTH_SHORT).show();
+                        recreate();
+                    }
+                });
+//                ibtnupdate = view.findViewById(R.id.ibtnupdate);
+//                String name = edtInput.getText().toString();
+//                ibtnupdate.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        databaseHandler.updateTravel(name,places, position);
+//                        recreate();
+//                    }
+//                });
+            }
+        });
 
 //         databaseHandler.saveCity(new City(1,"Đà lạt"));
 //        databaseHandler.saveCity(new City(2,"Buôn Ma Thuật"));
@@ -40,23 +91,9 @@ public class MainActivity extends AppCompatActivity {
 
        arrList =databaseHandler.getAllStudents();
 
-        adt= new CityAdapter(this, R.layout.item_layout,arrList);
+        adt= new ArrayAdapter(this, R.layout.item_layout, R.id.tvName,arrList);
+
         listView.setAdapter(adt);
-        btnSave = findViewById(R.id.btnSave);
-        txtName = findViewById(R.id.textCity);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name =txtName.getText().toString();
-                int max=arrList.size();
-                databaseHandler.saveCity(new City(max+1,name));
-                arrList =databaseHandler.getAllStudents();
-                adt= new CityAdapter(this, R.layout.item_layout,arrList);
-                listView.setAdapter(adt);
-            }
-        });
-        btnCancel = findViewById(R.id.btnCancel);
-
-
     }
+
 }
